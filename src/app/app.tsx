175bf5c7252ddcc8eb3740aa7ee2f034dc042e5d
bro <bro@beam-mw.com>
2024-12-27 15:07:25 +0100

    estimateGas

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -11,6 +11,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { MainContainer } from './containers/Main';
 import { ToastContainer } from 'react-toastify';
 import { Scrollbars } from 'react-custom-scrollbars';
+import { ethers } from "ethers";
 
 import './styles';
 
@@ -31,6 +32,27 @@ const routes = [
   }
 ];
 
+async function estimateGas() {
+  // Set up the EtherscanProvider
+  const provider = new ethers.providers.EtherscanProvider("homestead", "XKWU88XG5Z8ESF5KNKJNDH4VFZUWFUJD5N");
+
+  // Define the transaction
+  const tx = {
+    to: "0x959257a565d5Fb44724Bc322e83CAF8c8AaB3E8b", // Replace with the recipient's address
+    value: ethers.utils.parseEther("0.1"), // Replace with the amount of ETH to send
+    data: "0x", // Optional data payload
+  };
+
+  try {
+    // Estimate gas
+    const gasEstimate = await provider.estimateGas(tx);
+    alert(`Estimated Gas: ${gasEstimate.toString()}`);
+  } catch (error) {
+    console.error("Error estimating gas:", error);
+  }
+}
+
+
 const App = () => {
   const dispatch = useDispatch();
   const content = useRoutes(routes);
@@ -43,6 +65,10 @@ const App = () => {
       dispatch(sharedActions.navigate(''));
     }
   }, [navigateURL, dispatch, navigate]);
+
+  useEffect(() => {
+    estimateGas();
+  })
   
   return (
     <Scrollbars
